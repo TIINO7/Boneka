a1dd337a363cc76af2f80f46ff0ee309a24787ee
farai-coder <74096673+farai-coder@users.noreply.github.com>
2025-07-21 20:59:57 +0200

    Update orders_schema.py

--- schemas/orders_schema.py
+++ schemas/orders_schema.py
@@ -4,28 +4,29 @@ from pydantic import BaseModel
 from typing import Literal, Optional
 from uuid import UUID
 
-
 class OrderAction(BaseModel):
-    user_id : UUID
-    order_id : UUID
-    action : Literal["delivered","cancelled"]
-    
+    user_id: UUID
+    order_id: UUID
+    action: Literal["delivered", "cancelled"]
+
 class RequestInfo(BaseModel):
     id: UUID
     title: str
     description: Optional[str]
     category: Optional[str]
 
-    class Config:
-        orm_mode = True
-    
+    model_config = {
+        "from_attributes": True 
+    }
+
 class OrderOut(BaseModel):
     id: UUID
     status: str
     total_price: Decimal
     quantity: int
-    created_at: datetime
+    created_at: datetime.datetime  
     request: RequestInfo
 
-    class Config:
-        orm_mode = True
+    model_config = {
+        "from_attributes": True  
+    }
